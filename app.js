0ce409dc0bcd9346291d32ae37738d4ef73e26ec
charlene <charlene.xu92@gmail.com>
2023-08-09 11:25:57 -0700

    Get the home page to render the posts

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
 const _ = lodash;
 
 const app = express();
@@ -44,11 +43,16 @@ const postSchema = new mongoose.Schema({
 
 const Post = new mongoose.model("Post", postSchema);
 
-app.get("/", (req, res) => {
-  res.render("home.ejs", {
+app.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find({});
+    res.render("home.ejs", {
     homeContent: homeStartingContent ,
     posts: posts
-  });
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.get("/about", (req, res) => {
@@ -73,22 +77,24 @@ app.post("/compose", (req, res) => {
   res.redirect("/");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  const requestedTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postName", async (req, res) => {
+  try {
+    const requestedTitle = _.lowerCase(req.params.postName);
+    const posts = await Post.find({});
 
-  posts.forEach(post => {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-      res.render("post.ejs", {
-        title: post.title,
-        content: post.content
-      });
-    }
-  })
+    posts.forEach(post => {
+      const storedTitle = _.lowerCase(post.title);
+      if (storedTitle === requestedTitle) {
+        res.render("post.ejs", {
+          title: post.title,
+          content: post.content
+        });
+      }
+    })
+  } catch (err) {
+    console.error(err);
+  }
 });
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
