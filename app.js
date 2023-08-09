4c3cb2b02bb1a53722a1ecd8ffb8deb37f915281
charlene <charlene.xu92@gmail.com>
2023-08-09 11:02:48 -0700

    Save composed posts with Mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import lodash from "lodash";
+import mongoose from "mongoose";
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,6 +18,31 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+// Initialize Database
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+    console.log("Database initialization successful.");
+  } catch (err) {
+    console.error("Error initializing database:", err);
+  }
+}
+
+main();
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+const Post = new mongoose.model("Post", postSchema);
 
 app.get("/", (req, res) => {
   res.render("home.ejs", {
@@ -38,12 +64,12 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postContent,
-  };
+    content: req.body.postContent
+  });
 
-  posts.push(post);
+  post.save();
   res.redirect("/");
 });
 
